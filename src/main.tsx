2101e619d7580e19b7f554492fc2bb77a20772a5
mahdiconvoyages-cmd <mahdi.convoyages@gmail.com>
2025-10-17 17:44:07 +0200

    RESTORE: Desktop layout forcé sur mobile avec améliorations visibilité

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './styles/desktop-on-mobile.css';
 import './styles/accessibility-simple.css';
 
 createRoot(document.getElementById('root')!).render(
